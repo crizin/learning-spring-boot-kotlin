9a34e6a4e7524bc7d12501f8cd873752f60de65b
Crizin <crizin.avi@kakaocorp.com>
2018-10-19 19:15:01 +0900

    Add bootstrap, thymeleaf layout.

--- src/main/kotlin/net/crizin/learning/Application.kt
+++ src/main/kotlin/net/crizin/learning/Application.kt
@@ -1,14 +1,25 @@
 package net.crizin.learning
 
+import nz.net.ultraq.thymeleaf.LayoutDialect
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.transaction.annotation.EnableTransactionManagement
+import org.thymeleaf.TemplateEngine
+import org.thymeleaf.spring5.SpringTemplateEngine
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableTransactionManagement
-class Application
+class Application {
+	@Bean
+	fun templateEngine(): TemplateEngine {
+		val templateEngine = SpringTemplateEngine()
+		templateEngine.addDialect(LayoutDialect())
+		return templateEngine
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<Application>(*args)
